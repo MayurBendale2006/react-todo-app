0ed2ad3d1a0eb3d3bac5a023b7a3e1c8c6efeb0a
Mayur Bendale <mayurbendale2006@gmail.com>
2026-01-05 10:16:12 +0530

    Initial commit - Todo App

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import delete_icon from './delete.png'
+import tick_icon from './tick.png'
+import not_tick_icon from './not_tick.png'
+import todo_icon from './todo_icon.png'
+
+export const assets = {
+    delete_icon,
+    tick_icon,
+    not_tick_icon,
+    todo_icon
+}
